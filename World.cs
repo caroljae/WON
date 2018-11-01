eade1f8b1ba722efe46e743eb496e8f3ee73735b
Carol Goodnight <40777979+caroljae@users.noreply.github.com>
2018-11-01 16:14:36 -0500

    Add files via upload

---
+++ World.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World_of_Nations
+{
+    class World
+    {
+        //Declare and create on static World object
+        //that can be accessed from any other class
+        //Note: static objects are not in our textbooks
+        public static World theWorld = new World();
+
+        //Declare field
+        List<Nation> _nationList;
+
+        //Contructor
+        public World()
+        {
+            //Create empty list of nations
+            _nationList = new List<Nation>();
+        }
+
+        //Note: No properties
+
+        //Method to add a nation to the list
+        public void AddNation(Nation nat)
+        {
+            _nationList.Add(nat);
+        }
+
+        //Method to get information on all nations
+        public List<string> GetInfo()
+        {
+            //Create list of nation paragraphs
+            List<string> infoList = new List<string>();
+            //Add each nations info to the list
+            foreach(Nation nat in _nationList)
+            {
+                infoList.Add(nat.GetInfo());
+            }
+            //Return list to caller
+            return infoList;
+        }
+    }
+}
