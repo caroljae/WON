8de110e23516e4885f04a0896ef30cc1f7a96282
Carol Goodnight <40777979+caroljae@users.noreply.github.com>
2018-11-01 16:12:54 -0500

    Add files via upload

---
+++ DisplayForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace World_of_Nations
+{
+    partial class DisplayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayListBox = new System.Windows.Forms.ListBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // displayListBox
+            // 
+            this.displayListBox.FormattingEnabled = true;
+            this.displayListBox.Location = new System.Drawing.Point(12, 22);
+            this.displayListBox.Name = "displayListBox";
+            this.displayListBox.Size = new System.Drawing.Size(378, 173);
+            this.displayListBox.TabIndex = 0;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(169, 214);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 1;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // DisplayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 261);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.displayListBox);
+            this.Name = "DisplayForm";
+            this.Text = "Display Form";
+            this.Load += new System.EventHandler(this.DisplayForm_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox displayListBox;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
